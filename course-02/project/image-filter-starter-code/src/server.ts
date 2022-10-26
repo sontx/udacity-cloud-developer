ff6a33951fea084c2d383b71b12d9d805e3913b3
sontx <xuanson33bk@gmail.com>
2022-10-26 20:48:19 +0700

    Implement filteredimage api

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {deleteLocalFiles, filterImageFromURL, sendFile} from './util/util';
 
 (async () => {
 
@@ -12,6 +12,25 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
+
+  app.get('/filteredimage', async (req, res) => {
+    const {image_url} = req.query;
+    const isValidUrl = (url: string) => {
+      return url && /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(url);
+    }
+    if (!isValidUrl(image_url)) {
+      res.status(400).send('image_url must be an invalid url');
+    } else {
+      try {
+        const filteredImagePath = await filterImageFromURL(image_url);
+        await sendFile(filteredImagePath, res);
+        await deleteLocalFiles([filteredImagePath]);
+      } catch (e) {
+        console.log(e)
+        res.status(400).send('Sorry! we cannot process your image: ' + e.message)
+      }
+    }
+  })
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
